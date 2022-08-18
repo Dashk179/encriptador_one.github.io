fc14b61577e1e17d2320250b137e35a7b7341669
Arturo <dash_arturo@hotmail.com>
2022-08-18 17:55:38 -0500

    Logica arreglada

--- script.js
+++ script.js
@@ -1,50 +1,41 @@
-const textoIngresado = document.querySelector("#texto");
-const textoResultado = document.querySelector("#resulto");
+var resultado;
+function encriptar() {
+  const text1 = document.getElementById("texto").value.toLowerCase();
+  var texto = text1
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("o", "ober")
+    .replaceAll("a", "ai")
+    .replaceAll("u", "ufat");
 
-const btnEncriptar = document.querySelector("#boton_encriptado");
-const btnDesencriptar = document.querySelector("#desencri");
-const btnCopiar = document.querySelector("#copiar");
-
-function encriptar(){
-  let texto = textoIngresado.value.toLowerCase();
-  let textoEncriptado = texto
-  .replaceAll("e", "enter")
-  .replaceAll("i", "imes")
-  .replaceAll("o", "ober")
-  .replaceAll("a", "ai")
-  .replaceAll("u", "ufat");
-
-  document.getElementById("noResuelto").style.visibility = "hidden";
+    document.getElementById("noresuelto").style.visibility = "hidden";
+  document.getElementById("textresultado").value = texto;
+  document.getElementById("texto").value = "";
   document.getElementById("copiar").style.visibility = "visible";
-
-  textoResultado.value = textoEncriptado;
-
-  document.getElementById("texto").value = '';
+ 
 
 }
 
-function desencriptar (){
-  let textoEncriptado = textoIngresado.value.toLowerCase();
-  let texto = textoEncriptado
-  .replaceAll("enter", "e")
-  .replaceAll("imes", "i")
-  .replaceAll("ober", "o")
-  .replaceAll("ai", "a")
-  .replaceAll("ufat", "u");
+function desencriptar() {
+  const text2 = document.getElementById("textresultado").value.toLowerCase();
+  var texto2 = text2
+    .replaceAll( "enter","e")
+    .replaceAll( "imes","i")
+    .replaceAll( "ober","o")
+    .replaceAll( "ai","a")
+    .replaceAll( "ufat","u");
 
-  document.getElementById("noResuelto").style.visibility = "hidden";
-  document.getElementById("copiar").style.visibility = "visible";
+ 
+  document.getElementById("texto").value = texto2;
+  document.getElementById("textresultado").value = "";
+  document.getElementById("noresuelto").style.visibility = "disable";
 
-  textoResultado.value = texto;
-
-  document.getElementById("texto").value = '';
-
+ 
 }
 
-function copiar () {
-  let textoEncriptado = textoResultado.value;
-  navigator.clipboard.writeText(textoEncriptado);
+function copiar(){
+  const copiado = document.getElementById("textresultado").value;
+  navigator.clipboard.writeText(copiado);
+  alert("Usted a copiado: " + copiado);
+
 }
-
-
-
